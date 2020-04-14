f774be7c7e38d880e36006a9e24f2a8bfa67937a
thiKirill <k.a.yanushkin@gmail.com>
2020-04-14 15:58:27 +0300

    fix

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
